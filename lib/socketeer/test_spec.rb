9af70f48098928ffe5c40ec9b5ccdd917fef0f62
Robby Ranshous <rranshous@gmail.com>
2013-03-12 22:10:23 -0400

    refactored run code into test

--- lib/socketeer/test_spec.rb
+++ lib/socketeer/test_spec.rb
@@ -1,10 +1,35 @@
 require 'socket'
 require 'msgpack'
+require_relative '../lib/socketeer'
 
 #s = TCPSocket.new 'localhost', 2000
 #s.write MessagePack.pack "test"
 #s.write "\n\n"
 #s.close
+
+class Randomizer
+  include Socketeer
+  def handle_message message
+    message[:data] += Random.rand.to_s
+    message
+  end
+end
+
+rander = Randomizer.new
+rander.bind 'localhost', 2000
+
+echoer = Class.new.class_eval do
+  include Socketeer
+end.new
+
+echoer.bind 'localhost', 2001 do |m|
+  m
+end
+
+while loop
+  rander.cycle
+  echoer.cycle
+end
 
 s1 = TCPSocket.new 'localhost', 2000
 s2 = TCPSocket.new 'localhost', 2001
